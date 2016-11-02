bb61feeb57edb9c6e86853250259c9e646c2ea9b
tue39618 <tue39618@temple.edu>
2016-11-02 13:36:17 -0400

    added comments to EP_slurm.sh

--- EP_slurm.sh
+++ EP_slurm.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-#SBATCH -p all
-#SBATCH --array=1-1000
-#SBATCH --ntasks=1
-#SBATCH -t 48-00:00
-#SBATCH --mem-per-cpu=2000
+#SBATCH -p all  #the Queue or partition
+#SBATCH --array=1-1000 #the array size 
+#SBATCH --ntasks=1 #number of tasks to launch for each array item
+#SBATCH -t 48-00:00 #time H-M:Sec
+#SBATCH --mem-per-cpu=2000 #memory per cpu, cannot exceed 128GB for compute nodes or 256 for highmem
 #craig stanley
 #used to run EP using a slurm cluster (igem)
 
-module load openmpi
-val=$(sed "${SLURM_ARRAY_TASK_ID}q;d" $MPI_list)
-bash /home/tue39618/EP/run_EP.sh $val 
+module load openmpi #load openMPI
+
+val=$(sed "${SLURM_ARRAY_TASK_ID}q;d" $MPI_list) #pull input arguments
+bash /home/tue39618/EP/run_EP.sh $val  # excute megacc
